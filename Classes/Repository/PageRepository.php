d70dbae37bd1dba591cb7a253665dd0714d0cf47
Riad <riad@web-vision.de>
2025-12-18 12:57:46 +0100

    [TASK] Update PageRepository to exclude specific doktypes and fetch their children

--- Classes/Repository/PageRepository.php
+++ Classes/Repository/PageRepository.php
@@ -6,6 +6,7 @@ namespace WebVision\AiLlmsTxt\Repository;
 
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\Restriction\FrontendRestrictionContainer;
+use TYPO3\CMS\Core\Domain\Repository\PageRepository as CorePageRepository;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
@@ -35,12 +36,19 @@ class PageRepository
         return $result->fetchAssociative() ?: [];
     }
 
+    /**
+     * Doktypes that should be excluded but their children should still be fetched
+     */
+    private const EXCLUDED_DOKTYPES = [
+        CorePageRepository::DOKTYPE_RECYCLER,
+        CorePageRepository::DOKTYPE_SYSFOLDER,
+        CorePageRepository::DOKTYPE_SPACER,
+        CorePageRepository::DOKTYPE_SHORTCUT,
+    ];
+
     public function findNavigationByParent(int $parentUid): array
     {
-
         $queryBuilder = $this->connectionPool->getQueryBuilderForTable('pages');
-        $queryBuilder->getRestrictions()->removeAll()
-            ->add(GeneralUtility::makeInstance(FrontendRestrictionContainer::class));
 
         $result = $queryBuilder
             ->select('uid', 'pid', 'title', 'description', 'abstract', 'nav_title', 'doktype')
@@ -48,13 +56,24 @@ class PageRepository
             ->where(
                 $queryBuilder->expr()->eq('pid', $queryBuilder->createNamedParameter($parentUid)),
                 $queryBuilder->expr()->eq('nav_hide', $queryBuilder->createNamedParameter(0)),
-                $queryBuilder->expr()->eq('no_index', $queryBuilder->createNamedParameter(0))
+                $queryBuilder->expr()->eq('no_index', $queryBuilder->createNamedParameter(0)),
+                $queryBuilder->expr()->neq('doktype', $queryBuilder->createNamedParameter(CorePageRepository::DOKTYPE_RECYCLER)),
             )
             ->orderBy('sorting')
             ->executeQuery();
 
         $pages = [];
         while ($row = $result->fetchAssociative()) {
+            // Skip folders, spacers, and shortcuts but fetch their subpages
+            if (in_array((int)$row['doktype'], self::EXCLUDED_DOKTYPES, true)) {
+                // Recursively get children of skipped pages
+                $childPages = $this->findNavigationByParent((int)$row['uid']);
+                foreach ($childPages as $childPage) {
+                    $pages[] = $childPage;
+                }
+                continue;
+            }
+
             $pages[] = [
                 'uid' => $row['uid'],
                 'pid' => $row['pid'],
